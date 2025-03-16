0a1add6a9bfdae43abd01ebccec70fd387c56db9
Jake <jake.arefyev@gmail.com>
2025-03-16 22:18:28 +0200

    update packages

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
   name: "hello",
   dependencies: [
-    .package(url: "https://github.com/swiftwasm/carton", from: "1.1.2"),
-    .package(url: "https://github.com/swiftwasm/JavaScriptKit", from: "0.21.0"),
+    .package(url: "https://github.com/swiftwasm/carton", from: "1.1.3"),
+    .package(url: "https://github.com/swiftwasm/JavaScriptKit", from: "0.23.0"),
   ],
   targets: [
     .executableTarget(
